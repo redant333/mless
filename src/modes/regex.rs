5547d98dc7d0932d226d850feb9fb045d98cb331
Milomir Babic <redant333@gmail.com>
2024-08-02 14:09:05 +0200

    Fix ignoring of ANSI sequences in regex mode

--- src/modes/regex.rs
+++ src/modes/regex.rs
@@ -60,24 +60,33 @@ impl RegexMode {
         // match text. Instead, every occurrence of the same match should get the
         // same hint since it will give the same output.
 
-        // If there is an ANSI color sequence before the match, it should be ignored.
-        // For that reason, add the ignore regex to the beginning and the end of the
-        // regex being searched with a ? operator. If it's there, it won't be included
-        // in the main match, if it isn't, it will just be ignored.
-        // This can be extended for anything else that needs to be ignored.
-        let ignore_regex = "\x1b\\[[^m]+m".to_string();
+        // All ANSI color sequences should be ignored while matching
+        let ignore_regex = Regex::new("\x1b\\[[^m]+m").unwrap();
+
+        let ignore_ranges = ignore_regex
+            .captures_iter(data)
+            .map(|captures| {
+                let regex_match = captures.get(0).unwrap();
+                (regex_match.start(), regex_match.end())
+            })
+            .collect::<Vec<(usize, usize)>>();
+
+        // Remove all the ignored sequences and perform the matching
+        // on the resulting data
+        let cleaned_data = ignore_regex.replace_all(data, "");
 
         for regex in &args.regexes {
-            let wrapped_regex = format!("({ignore_regex})?({regex})({ignore_regex})?");
-            let regex = Regex::new(&wrapped_regex).unwrap();
+            let regex = Regex::new(regex).unwrap();
 
             regex
-                .captures_iter(data)
+                .captures_iter(&cleaned_data)
                 .filter_map(|capture| {
-                    let regex_match = capture.get(2)?;
+                    let regex_match = capture.get(0)?;
 
+                    let start_before_ignored =
+                        get_original_index(&ignore_ranges, regex_match.start());
                     Some(Hit {
-                        start: regex_match.start(),
+                        start: start_before_ignored,
                         length: regex_match.as_str().len(),
                         text: regex_match.as_str().to_string(),
                     })
@@ -161,9 +170,38 @@ impl Mode for RegexMode {
     }
 }
 
+/// For a sequence from which `removed_ranges` where removed, find the index that
+/// the element had before removal for the given `index_after_removal`.
+///
+/// `removed_ranges` represents the collection of ranges (a, b) where a is included
+/// in the range and b is not.
+///
+/// ```
+/// // before removal [0, 1, 2, 3, 4, 5, 6]
+/// // after removal  [0, 2, 3, 6]
+/// let removed_ranges = [(1,2), (4,6)];
+/// let index_after_removal = 3;
+///
+/// assert_eq!(get_original_index(removed_ranges, index_after_removal), 6);
+/// ```
+fn get_original_index(removed_ranges: &[(usize, usize)], index_after_removal: usize) -> usize {
+    let mut offset_due_to_removed = 0;
+
+    for &(start, end) in removed_ranges {
+        if index_after_removal + offset_due_to_removed < start {
+            return index_after_removal + offset_due_to_removed;
+        }
+
+        offset_due_to_removed += end - start;
+    }
+
+    index_after_removal + offset_due_to_removed
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{configuration::RegexArgs, hints::MockHintGenerator};
+    use test_case::test_case;
 
     use super::*;
 
@@ -220,23 +258,49 @@ mod tests {
 
     #[test]
     fn produces_expected_highlights_and_overlays_for_colored_text() {
+        let color_1 = "\x1b[0;31m";
+        let color_2 = "\x1b[0;32m";
+        let color_reset = "\x1b[0m";
+
         let (text_overlays, styled_segments) = get_draw_instructions(
-            "\x1b[94mthings\x1b[0m and \x1b[93mstuff\x1b[0m",
+            &format!("{color_1}things{color_reset} and {color_2}stuff{color_reset} and {color_1}mice{color_reset} and {color_2}mud{color_reset}"),
             vec![r"[a-z]{4,}".into()],
-            vec!["a".into(), "b".into()],
+            vec!["a".into(), "b".into(), "c".into()],
         );
 
-        assert_eq!(text_overlays.len(), 2);
-        assert!(has_overlay_at_location(&text_overlays, 5));
-        assert!(has_overlay_at_location(&text_overlays, 25));
+        println!("{:#?}", text_overlays);
+        println!("{:#?}", styled_segments);
 
-        assert_eq!(styled_segments.len(), 4);
+        assert_eq!(text_overlays.len(), 3);
+        assert!(has_overlay_at_location(&text_overlays, 7));
+        assert!(has_overlay_at_location(&text_overlays, 29));
+        assert!(has_overlay_at_location(&text_overlays, 50));
+
+        assert_eq!(styled_segments.len(), 6);
         // Highlights for "things" match
-        assert!(has_highlight(&styled_segments, 5, 6));
-        assert!(has_highlight(&styled_segments, 5, 1));
+        assert!(has_highlight(&styled_segments, 7, 6));
+        assert!(has_highlight(&styled_segments, 7, 1));
 
         // Highlights for "stuff" match
-        assert!(has_highlight(&styled_segments, 25, 5));
-        assert!(has_highlight(&styled_segments, 25, 1));
+        assert!(has_highlight(&styled_segments, 29, 5));
+        assert!(has_highlight(&styled_segments, 29, 1));
+
+        // Highlights for "mice" match
+        assert!(has_highlight(&styled_segments, 50, 4));
+        assert!(has_highlight(&styled_segments, 50, 1));
+    }
+
+    #[test_case(&[(2,4), (6, 8)], 0, 0)]
+    #[test_case(&[(2,4), (6, 8)], 1, 1)]
+    #[test_case(&[(2,4), (6, 8)], 2, 4)]
+    #[test_case(&[(2,4), (6, 8)], 3, 5)]
+    #[test_case(&[(2,4), (6, 8)], 4, 8)]
+    #[test_case(&[], 4, 4)]
+    fn get_original_index_returns_correct_value(
+        removed_ranges: &[(usize, usize)],
+        index: usize,
+        expected: usize,
+    ) {
+        assert_eq!(get_original_index(removed_ranges, index), expected);
     }
 }
