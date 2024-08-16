430985c81e8d47cfa23dde37f7b87745a33acb47
Milomir Babic <redant333@gmail.com>
2024-08-16 15:31:34 +0200

    Make sure that pressing Ctrl+C does not leave terminal in a weird state

--- tmux/mless_tmux_stage_2.sh
+++ tmux/mless_tmux_stage_2.sh
@@ -10,7 +10,11 @@ cmd="mouseless-selector $select_from_file"
 
 tmux swap-pane -s "$selection_source_pane_id" -t "$picker_pane_id"
 
-$cmd | tmux loadb -b mless-buff - && tmux paste-buffer -b mless-buff -t "$selection_source_pane_id"
+selected_text=$($cmd)
+
+if [[ "$selected_text" != "" ]]; then
+    echo -n "$selected_text" | tmux loadb -b mless-buff - && tmux paste-buffer -b mless-buff -t "$selection_source_pane_id"
+fi
 
 tmux swap-pane -s "$picker_pane_id" -t "$selection_source_pane_id"
 tmux kill-window -t "$side_window_id"
